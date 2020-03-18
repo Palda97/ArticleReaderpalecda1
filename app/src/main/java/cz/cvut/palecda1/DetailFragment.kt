897db8ce60aabdfe910d6303f7df17bc67b346f1
David Paleček <palecda1@fit.cvut.cz>
2020-03-18 07:38:51 +0100

    funkcionalita done

--- app/src/main/java/cz/cvut/palecda1/DetailFragment.kt
+++ app/src/main/java/cz/cvut/palecda1/DetailFragment.kt
@@ -1,33 +1,77 @@
 package cz.cvut.palecda1
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.Html
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
+import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.app.ShareCompat
+import androidx.core.view.MenuItemCompat
 import androidx.fragment.app.Fragment
 import java.util.*
+
+import androidx.appcompat.app.AppCompatActivity
 
 class DetailFragment : Fragment() {
 
     private lateinit var articleTextView: TextView
     //var listener: DetailFragmentListener? = null
+    private var myid: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_detail, container, false)
 
         articleTextView = view.findViewById(R.id.textViewForArticle) as TextView
 
-        val id = Objects.requireNonNull<Bundle>(arguments).getInt(ID)
-        showArticle(id)
+        myid = Objects.requireNonNull<Bundle>(arguments).getInt(ID)
+        //val id = Objects.requireNonNull<Bundle>(arguments).getInt(ID)
+        showArticle()
+        //showArticle(id)
+
 
         return view
     }
 
-    fun showArticle(id: Int){
+    fun showArticle(){
+        articleTextView.text = Html.fromHtml(getText())
+    }
+    /*fun showArticle(id: Int){
         articleTextView.text = Html.fromHtml(DataStorage.articleArray[id])
+    }*/
+    fun getText(): String{
+        return DataStorage.articleArray[myid]
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.my_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            return when (item.itemId) {
+                R.id.shareItem -> {
+                    val sharingIntent = Intent(Intent.ACTION_SEND)
+                    sharingIntent.setType("text/plain")
+                    val body = getText()
+                    //val subject = ""
+                    sharingIntent.putExtra(Intent.EXTRA_TEXT, body)
+                    //sharingIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+                    startActivity(Intent.createChooser(sharingIntent, "Share Using"))
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }else
+            return super.onOptionsItemSelected(item)
     }
 
     /*override fun onAttach(context: Context?) {
@@ -44,7 +88,7 @@ class DetailFragment : Fragment() {
     }
 
     interface DetailFragmentListener {
-        //fun showDetail(id: Int)
+        fun getAc(): AppCompatActivity
     }*/
 
     companion object {
