53be2639e3d4380fc60c784cc08adc0b9d7bb9b5
David Paleček <palecda1@fit.cvut.cz>
2020-05-13 15:21:12 +0200

    scrolling corutine

--- app/src/main/java/cz/cvut/palecda1/view/fragment/DetailFragment.kt
+++ app/src/main/java/cz/cvut/palecda1/view/fragment/DetailFragment.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.*
+import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -17,7 +18,7 @@ import cz.cvut.palecda1.R
 import cz.cvut.palecda1.databinding.FragmentDetailBinding
 import cz.cvut.palecda1.view.HtmlFactory
 import cz.cvut.palecda1.viewmodel.ArticleViewModel
-import java.util.Objects
+import kotlinx.coroutines.*
 
 class DetailFragment : Fragment() {
 
@@ -32,15 +33,21 @@ class DetailFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         //val view = inflater.inflate(R.layout.fragment_detail, container, false)
 
         articleId = arguments?.getString(ID, null)
 
         //MVVM init + databinding
 
-        binding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_detail, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_detail, container, false
+        )
         val view = binding.root
 
         binding.detailTextView.movementMethod = LinkMovementMethod.getInstance()
@@ -49,8 +56,9 @@ class DetailFragment : Fragment() {
         articleId?.let { viewModel.loadArticleById(it) }
 
         viewModel.roomArticleLiveData.observe(this, Observer {
-            if (it != null && it.isOk){
+            if (it != null && it.isOk) {
                 binding.article = HtmlFactory.toHtml(it.mailContent!!.title, it.mailContent.body)
+                scrollAfter(200)
             }
             binding.mail = it
             binding.executePendingBindings()
@@ -61,13 +69,29 @@ class DetailFragment : Fragment() {
         return view
     }
 
+    private suspend fun scrollOnMain() {
+        withContext(Dispatchers.Main) {
+            //binding.scrollViewInDetail.fling(-10000)
+            //binding.scrollViewInDetail.fullScroll(ScrollView.FOCUS_UP)
+            binding.scrollViewInDetail.smoothScrollTo(0, 0)
+        }
+    }
+
+    private fun scrollAfter(ms: Int) {
+        suspend fun scrolling(waitTime: Int) {
+            delay(waitTime.toLong())
+            scrollOnMain()
+        }
+        CoroutineScope(Dispatchers.IO).launch { scrolling(ms) }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_detail, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
     private fun shareCurrentArticle() {
-        if(articleId == null) {
+        if (articleId == null) {
             context?.let { Injector.noArticleSelectedToast(it) }
             return
         }
@@ -81,14 +105,18 @@ class DetailFragment : Fragment() {
     }
 
     private fun openLink() {
-        if(articleId == null){
+        if (articleId == null) {
             context?.let { Injector.noArticleSelectedToast(it) }
             return
         }
         try {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(articleId)))
-        }catch (e: ActivityNotFoundException){
-            Toast.makeText(context!!, "<$articleId> " + getString(R.string.is_not_a_valid_url), Toast.LENGTH_SHORT).show()
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                context!!,
+                "<$articleId> " + getString(R.string.is_not_a_valid_url),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
